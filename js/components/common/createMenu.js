f9c2ce3c5f98d8d43b8ad7f587121eb635edf1e2
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-02-25 18:18:16 +0100

    logged in

--- js/components/common/createMenu.js
+++ js/components/common/createMenu.js
@@ -1,8 +1,28 @@
+import { getUsername } from "../../utils/storage.js";
+
 export default function createMenu() {
 	const container = document.querySelector(".menu-container");
 
+	const { pathname } = document.location;
+	const username = getUsername();
+
+	let authLink = `<a href="login.html" class="${
+		pathname === "/moises-magalhaes-js2-ma3/login.html" ? "active" : ""
+	}"></a>`;
+
+	if (username) {
+		authLink = `<span>Hi! ${username}</span>`;
+	}
+	console.log(username);
+	console.log(pathname);
+
 	container.innerHTML = `<div class="menu">
-                                <a href="index.html">Home</a>
-                                <a href="login.html">Login</a>
+                                <a href="index.html" class= "${
+																	pathname ===
+																	"/moises-magalhaes-js2-ma3/index.html"
+																		? "active"
+																		: ""
+																}">Home</a>
+                                ${authLink}
                           </div>`;
 }
